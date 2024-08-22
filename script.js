b9475a7fb9e4efc0cd8e48002a8f22d60be3f981
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-08-22 18:42:21 -0400

    refactor: code improvement

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ let botao = document.getElementById("calcular");
 let res = document.getElementById("resultado");
 
 function calcular(n1, n2) {
-  if (typeof n1 == 'number' && typeof n2 == 'number') {
-    return n1 + n2;
-  }else {
-    n1 = parseInt(n1);
-    n2 = parseInt(n2);
-    return n1 + n2;
-  }
+  return Number(n1) + Number(n2);
 }
 
 botao.addEventListener("click", function () {
